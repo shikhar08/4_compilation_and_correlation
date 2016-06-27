76cce0e26b59ae1150a4b8194079b08e3bb1b3a8
Shikhar Srivastava <shikhar.srivastava@outlook.com>
2016-06-27 09:57:43 +0200

    Updated-Comments

--- 4_compilation.r
+++ 4_compilation.r
@@ -1,4 +1,5 @@
 
+
 rm(list=ls())
 
 user=(Sys.info()[6])
@@ -6,43 +7,48 @@ Desktop=paste("C:/Users/",user,"/Desktop/",sep="")
 setwd(Desktop)
 
 
-home=paste(Desktop,"MEMS/S3/Seminar-R&U/ForestFire/",sep="")
+home=paste(Desktop,"MEMS/S4/R Programming/German/",sep="")
 
-setwd(paste(home,'ForestFireModel',sep=''))
+setwd(paste(home,'Model',sep=''))
+preproc_dir=paste(home,'Model',"/","preproc_data",sep='')
 
-	preproc_dir=paste(home,'ForestFireModel',"/","preproc_data",sep='')
-	dir.create("candidates_val")
-	candidates_val=paste(home,'ForestFireModel',"/","candidates_val",sep='')
-	dir.create("candidates_test")
-	candidates_test=paste(home,'ForestFireModel',"/","candidates_test",sep='')
-	
-	setwd(candidates_val)
-	myFiles <- list.files(pattern="*csv")
-	
-	library(plyr)
-	import <- llply(myFiles, read.csv)
-	b=do.call("cbind", import)
-	
+candidates_val=paste(home,'Model',"/","candidates_val",sep='')
 
-	setwd(preproc_dir)
-	c=read.csv("val.csv",as.is=T)
-	b=cbind(c[,c("id","dv_binary")],b)
-	setwd(candidates_val)
-	write.csv(b,"all_val.csv",row.names=F)
-	col=gsub('val','test',colnames(b))
+candidates_test=paste(home,'Model',"/","candidates_test",sep='')
 	
-	
-	setwd(candidates_test)
-	myFiles <- list.files(pattern="*csv")
+setwd(candidates_val)
+myFiles = list.files(pattern="*csv")
 
-	
-	import <- llply(myFiles, read.csv)
-	b=do.call("cbind", import)
+library(plyr)
+##Importing all the model csv files
+import = llply(myFiles, read.csv)
+## Mass cbind
+b=do.call("cbind", import)
 
-	setwd(preproc_dir)
-	c=read.csv("test.csv",as.is=T)
-	b=cbind(c[,c("id","dv_binary")],b)
-	setwd(candidates_test)
-	b=b[,col]
-	write.csv(b,"all_test.csv",row.names=F)
+
+setwd(preproc_dir)
+c=read.csv("val.csv",as.is=T)
+b=cbind(c[,c("id","dv")],b)
+setwd(candidates_val)
+
+#Saving all
+write.csv(b,"all_val.csv",row.names=F)
+
+#in test samples column names have "test" string as against "val" in validation samples
+col=gsub('val','test',colnames(b))
+
+
+setwd(candidates_test)
+myFiles = list.files(pattern="*csv")
+
+
+import = llply(myFiles, read.csv)
+b=do.call("cbind", import)
+
+setwd(preproc_dir)
+c=read.csv("test.csv",as.is=T)
+b=cbind(c[,c("id","dv")],b)
+setwd(candidates_test)
+b=b[,col]
+write.csv(b,"all_test.csv",row.names=F)
 
